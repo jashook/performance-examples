9587fd8f9eeba0c6b3912bac3a2077985fce632c
Jarret Shook <jshook@roblox.com>
2023-02-06 20:15:20 -0800

    Simple tuple example

---
+++ structs/tuple/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+var summary = BenchmarkRunner.Run<ValueTupleBenchmark>();
+
+[MemoryDiagnoser]
+public class ValueTupleBenchmark
+{
+    [Benchmark]
+    public void TupleExample()
+    {
+        Tuple<int, int> tup = new Tuple<int, int>(1, 2);
+    }
+
+    [Benchmark]
+    public void ValueTupleExample()
+    {
+        ValueTuple<int, int> tup = (3, 4);
+    }
+
+    [Benchmark]
+    public void TupleCreatedInLoop()
+    {
+        for (int index = 0; index < 1000000; ++index)
+        {
+            Tuple<int, int> tup = new Tuple<int, int>(1, 2);
+        }
+    }
+
+    [Benchmark]
+    public void ValueTupleCreatedInLoop()
+    {
+        for (int index = 0; index < 1000000; ++index)
+        {
+            ValueTuple<int, int> tup = (3, 4);
+        }
+    }
+}
